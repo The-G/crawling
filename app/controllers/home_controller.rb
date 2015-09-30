b9aa390b7c8fdb31cf75f1352c80a1260937ffb7
Ji Hyuk Kim <gskjhj@naver.com>
2015-09-30 14:16:10 +0000

    edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,23 +4,30 @@ class HomeController < ApplicationController
   
   def crawling
     require 'open-uri'
-        @search = params[:want]
-        @url = URI.encode("http://cafeblog.search.naver.com/search.naver?where=post&sm=tab_jum&ie=utf8&query=#{@search}")
-=begin
-        field = Nokogiri::HTML(open(data))
-        field0 = field.css("ul > li:nth-child(#{j}) > p")[0].text
-        title = field.css("ul > li:nth-child(#{j}) > div.main-info.clearfix > h4 > a:nth-child(2)").text
-        textnum = field.css("ul > li:nth-child(#{j}) > div.main-info.clearfix > h4 > a:nth-child(2)")[0]['href']
-        #dday = field.css("ul > li:nth-child(1) > div.main-info.clearfix > p > span").text
-        comdate = field.css("ul > li:nth-child(#{j}) > div.main-info.clearfix > p").text
-    
-        competition = Competition.new
-        competition.title = title
-        competition.textnum = textnum
-        competition.field = field0
-        #competition.d_day = dday
-        competition.com_date = comdate
+        @want = params[:want]
+        @url_article = URI.encode("http://cafeblog.search.naver.com/search.naver?sm=tab_hty.top&where=article&ie=utf8&query=#{params[:want]}+-#{params[:no_want]}")
+        @url_post = URI.encode("http://cafeblog.search.naver.com/search.naver?sm=tab_hty.top&where=post&ie=utf8&query=#{params[:want]}+-#{params[:no_want]}")
+        article = Nokogiri::HTML(open(@url_article)) #카페
+        postt = Nokogiri::HTML(open(@url_post)) #블로그
         
+
+        for i in 1..10
+          search = Search.new
+          search.url = article.css("#elThumbnailResultArea > li:nth-child(#{i}) > dl > dt > a")[0]['href']
+          search.title = article.css("#elThumbnailResultArea > li:nth-child(#{i}) > dl > dt > a")[0].text
+          search.save
+        end
+        for i in 1..10
+          search = Search.new
+          search.url = postt.css("#elThumbnailResultArea > li:nth-child(#{i}) > dl > dt > a")[0]['href']
+          search.title = postt.css("#elThumbnailResultArea > li:nth-child(#{i}) > dl > dt > a")[0].text
+          search.save
+        end
+        
+        @search = Search.all
+#@test = Search.first.title.split(',')[1] #array 사용시
+=begin    
+        search = Search.new
         data2 = URI("http://www.detizen.com/contest/#{textnum}")
         field2 = Nokogiri::HTML(open(data2))
         image = field2.css("#Main > section:nth-child(3) > div > div > div.top_info.after > div.poster > img")[0]['src']
@@ -28,9 +35,8 @@ class HomeController < ApplicationController
         
         competition.url = field2.css("#Main > section:nth-child(3) > div > div > div.top_info.after > div.contest-info > table > tbody > tr:nth-child(9) > td > a")[0]["href"]
        
-        
-        #competition.content =
         competition.save
 =end
+
   end  
 end
